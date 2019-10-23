92f2dc6a1e8b6aef667d68329f91d04c8bee6deb
iprih <priscila.silva076@gmail.com>
2019-10-22 21:32:13 -0300

    adicionando quem concluiu tarefa

--- src/controllers/tarefasController.js
+++ src/controllers/tarefasController.js
@@ -13,3 +13,14 @@ exports.getById = (req,res) => {
     const id = req.params.id
     res.status(200).send(tarefas.find(item => item.id == id))
 }
+
+//criar pessoas que tenham concluido a tarefa
+
+
+exports.getConcluido = (req, res) => {
+    
+    const tarefaConcluida = tarefas.filter(item => item.concluido == "true")
+    const tarefaConcluidaa = tarefaConcluida.map(item => item.funcionario)
+    
+    res.status(200).send(tarefaConcluidaa)
+}
--- src/routes/tarefasRoute.js
+++ src/routes/tarefasRoute.js
@@ -3,7 +3,11 @@ const router = express.Router()
 const controller = require("../controllers/tarefasController")
 
 router.get("/", controller.get)
+
+router.get("/teste",controller.getConcluido)
 //colocar rota do id
 router.get("/:id",controller.getById)
+
+
 
 module.exports = router
